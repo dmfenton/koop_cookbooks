47cac240ac97ab0cd52c755a20add86a9f8594a3
Daniel Fenton <dfenton@esri.com>
2016-04-20 16:17:21 -0400

    Update koop_deploy.rb

--- koop_app_server/recipes/koop_deploy.rb
+++ koop_app_server/recipes/koop_deploy.rb
@@ -14,22 +14,6 @@ node[:deploy].each do |application, deploy|
     group 'root'
     mode 0644
   end
- 
-  template'worker alarm' do
-    cookbook 'koop_app_server'
-    path "#{deploy[:current_path]}/worker-alarm.sh"
-    source 'worker-alarm.sh.erb'
-    owner 'root'
-    group 'root'
-    mode 0744
-  end
-  
-  cron "run_worker_alarm" do
-    hour "*"
-    minute "*/1"
-    weekday "*"
-    command "#{deploy[:current_path]}/worker-alarm.sh"
-  end
 
   execute 'stop monit' do
     cwd "/"
@@ -41,7 +25,7 @@ node[:deploy].each do |application, deploy|
     group 'root'
     user 'root'
     cwd "#{deploy[:current_path]}"
-    command 'pm2 delete all'
+    command 'pm2 kill'
     ignore_failure false
   end
 
@@ -51,12 +35,5 @@ node[:deploy].each do |application, deploy|
     ignore_failure false
   end
 
-  #ruby_block "restart node.js application #{application}" do
-  #  block do
-  #    Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
-  #    Chef::Log.info(`#{node[:deploy][application][:nodejs][:restart_command]}`)
-  #    $? == 0
-  #  end
-  #end
 
 end
